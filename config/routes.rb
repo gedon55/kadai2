f752e95f84cae1e3b0e768fb16faa964d57f9508
gedon55 <108569235+gedon55@users.noreply.github.com>
2022-07-15 15:40:05 +0000

    break

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get '/new' => 'books#new'
-  get '/index' => 'books#index'
-  get '/show' => 'books#show'
-  get '/edit' => 'books#edit'
+  get '/books' => 'books#index'
+  post 'books' => 'books#create'
   get '/top' => 'homes#top'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
