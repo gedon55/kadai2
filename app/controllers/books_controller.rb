438044e0d505e3f49bfe72cb2ebaf1e6df53c4f9
gedon55 <108569235+gedon55@users.noreply.github.com>
2022-07-16 14:33:54 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,13 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      @books=Book.all
+      render :index
+    end
   end
 
   def show
@@ -22,9 +26,19 @@ class BooksController < ApplicationController
   end
   
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated'
+    else
+      @books=Book.all
+      render :edit
+    end
+  end
+  
+  def destroy
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    book.destroy
+    redirect_to '/books', alert:'Book was successfully destroyed.'
   end
 
  private
